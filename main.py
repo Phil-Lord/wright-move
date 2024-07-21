f1c8d464740a2cdd5c4e617864d64e30aa08c975
Phil Lord <phillord2010@gmail.com>
2024-07-21 23:41:07 +0100

    Add main function

---
+++ main.py
@@ -0,0 +1,10 @@
+from get_details import get_linley_and_simpson_properties
+
+
+def main():
+    details = get_linley_and_simpson_properties()
+    print(details)
+
+
+if __name__ == '__main__':
+    main()
