c45a6c0d0dfbb24832d095e836fb5b0815db7376
Phil Lord <phillord2010@gmail.com>
2026-05-19 18:45:57 +0100

    Add header component w/ sort by

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,28 @@
+import { SORT_OPTIONS, type SortOption } from './sort'
+
+interface HeaderProps {
+  sort: SortOption
+  onSortChange: (sort: SortOption) => void
+}
+
+export function Header({ sort, onSortChange }: HeaderProps) {
+  return (
+    <header className="header">
+      <h1>Wright Move</h1>
+      <label className="sort">
+        <span className="sort-label">Sort by</span>
+        <select
+          className="sort-select"
+          value={sort}
+          onChange={(e) => onSortChange(e.target.value as SortOption)}
+        >
+          {SORT_OPTIONS.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </label>
+    </header>
+  )
+}
